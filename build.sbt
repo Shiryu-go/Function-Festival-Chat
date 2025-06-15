9dc6b8bbeaf0cabc150420bcc9f539d085338e97
Shiryu_Go <ilovenhunhu777@gmail.com>
2025-06-15 14:16:35 +0900

    WebSocketを複数繋げてちゃんとチャットっぽくなるようにしました、ようやくScalaの真髄を味わえるスタート地点？
    build.sbtとMain.scalaの依存関係を更新し、WebSocketルーティングを改善しました
    型が難しすぎる、monadとかscopとか

--- build.sbt
+++ build.sbt
@@ -1,21 +1,14 @@
-ThisBuild / version := "0.1.0-SNAPSHOT"
+scalaVersion := "3.3.6"
 
-ThisBuild / scalaVersion := "3.3.6"
-
-lazy val root = (project in file("."))
-  .settings(
-    name := "Function-Festival-Chat"
-  )
-val http4sVersion = "0.23.24"
-val zioVersion = "2.0.19"
+val zioVersion    = "2.1.19"
+val http4sVersion = "0.23.30"
 
 libraryDependencies ++= Seq(
-  "dev.zio" %% "zio" % zioVersion,
-  "dev.zio" %% "zio-streams" % zioVersion,
-  "dev.zio" %% "zio-interop-cats" % "23.1.0.1",
-  "org.http4s" %% "http4s-dsl" % http4sVersion,
-  "org.http4s" %% "http4s-ember-server" % http4sVersion, // ← blaze → ember に変更
-  "org.http4s" %% "http4s-circe" % http4sVersion,
-  "io.circe" %% "circe-generic" % "0.14.5",
-  "io.circe" %% "circe-parser" % "0.14.5"
+  "dev.zio" %% "zio"              % zioVersion,
+  "dev.zio" %% "zio-streams"      % zioVersion,
+  "dev.zio" %% "zio-interop-cats" % "23.1.0.5",
+  "org.http4s" %% "http4s-dsl"          % http4sVersion,
+  "org.http4s" %% "http4s-ember-server" % http4sVersion,
+  "org.http4s" %% "http4s-server"       % http4sVersion,
+  "com.comcast" %% "ip4s-core"          % "3.7.0"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,43 +1,50 @@
-import com.comcast.ip4s.Literals.{ipv4, port}
 import com.comcast.ip4s.{ipv4, port}
-import fs2.*
-import zio.*
-import zio.interop.catz.*
-import org.http4s.*
-import org.http4s.dsl.*
-import org.http4s.ember.server.*
-import org.http4s.server.Router
-import org.http4s.server.websocket.WebSocketBuilder2
+import zio._
+import zio.interop.catz._
+import fs2.Stream
+import org.http4s._
+import org.http4s.dsl.Http4sDsl
 import org.http4s.websocket.WebSocketFrame
+import org.http4s.server.websocket.WebSocketBuilder2
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.Router
+import org.http4s.server.middleware.Logger
+import org.http4s.implicits._
 
 object Main extends ZIOAppDefault {
 
-  type MyIO[A] = RIO[Any, A]
+  private type F[A] = ZIO[Scope, Throwable, A]
+  private object dsl extends Http4sDsl[F]; import dsl._
 
-  object dsl extends Http4sDsl[MyIO]
-  import dsl.*
-
-  val echoPipe: Pipe[MyIO, WebSocketFrame, WebSocketFrame] =
-    _.collect {
-      case WebSocketFrame.Text(msg, _) => WebSocketFrame.Text(s"Echo: $msg")
-    }
-
-  def chatRoutes(wsBuilder: WebSocketBuilder2[MyIO]): HttpRoutes[MyIO] =
-    HttpRoutes.of[MyIO] {
+  // ---------------------------------------------------------------------
+  // ① Builder を引数で受け取る
+  private def routes(hub: Hub[String])(wsb: WebSocketBuilder2[F]): HttpRoutes[F] =
+    HttpRoutes.of[F] {
       case GET -> Root / "chat" =>
-        wsBuilder.build(echoPipe)
+        for {
+          dq  <- hub.subscribe
+          in   = (stream: Stream[F, WebSocketFrame]) =>
+            stream.collect { case WebSocketFrame.Text(t, _) => t }
+              .evalMap(msg => hub.publish(msg).unit)
+          out  = Stream.repeatEval(dq.take).map(WebSocketFrame.Text(_))
+          resp <- wsb.build(out, in)          // ここでレスポンスを組み立てる
+        } yield resp
     }
-
-  override val run: ZIO[Any, Throwable, ExitCode] = for {
-    runtime <- ZIO.runtime[Any]
-    _ <- EmberServerBuilder
-      .default[MyIO]
-      .withHost(ipv4"0.0.0.0")
-      .withPort(port"8080")
-      .withHttpWebSocketApp(wsBuilder =>
-        Router("/" -> chatRoutes(wsBuilder)).orNotFound
-      )
-      .build
-      .useForever
-  } yield ExitCode.success
+  override def run: ZIO[ZIOAppArgs & Scope, Throwable, Unit] =
+    ZIO.scoped {                              // ★ Scope を開く
+      for {
+        hub <- Hub.unbounded[String]
+        _   <- EmberServerBuilder
+          .default[F]
+          .withHost(ipv4"0.0.0.0")
+          .withPort(port"8080")
+          // ② Builder を渡せるエントリーポイント
+          .withHttpWebSocketApp { wsb =>
+            val app = Router("/" -> routes(hub)(wsb)).orNotFound
+            Logger.httpApp(true, true)(app)
+          }
+          .build
+          .useForever                  // Resource が Scope を要求
+      } yield ()
+    }
 }
